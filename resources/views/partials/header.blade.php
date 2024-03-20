3174a22ac30532f4240fd0c4e6a62c17caf258ba
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-03-20 10:07:53 +0100

    add controller and model

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,7 +9,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a aria-current="page" class="nav-link active" href="#">Home</a>
+            <a @class(['nav-link', 'active' => Route::currentRouteName() == 'home']) aria-current="page" href="{{ route('home') }}">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
